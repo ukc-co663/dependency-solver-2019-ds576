1daff3ccf166efc6203e1698175f2c5bc2fef504
Radu Grigore <radugrigore@gmail.com>
2018-02-05 14:17:21 +0000

    parse "depends"

--- src/depsolver/Main.java
+++ src/depsolver/Main.java
@@ -8,28 +8,22 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-class Dependency {
-  private List<String> alternatives = new ArrayList<>();
-  public List<String> getAlternatives() { return alternatives; }
-  public void setAlternatives(List<String> alternatives) { this.alternatives = alternatives; }
-}
-
 class Package {
   private String name;
   private String version;
   private Integer size;
-  private List<Dependency> dependencies = new ArrayList<>();
+  private List<List<String>> depends = new ArrayList<>();
   private List<String> conflicts = new ArrayList<>();
 
   public String getName() { return name; }
   public String getVersion() { return version; }
   public Integer getSize() { return size; }
-  public List<Dependency> getDependencies() { return dependencies; }
+  public List<List<String>> getDepends() { return depends; }
   public List<String> getConflicts() { return conflicts; }
   public void setName(String name) { this.name = name; }
   public void setVersion(String version) { this.version = version; }
   public void setSize(Integer size) { this.size = size; }
-  public void setDependencies(List<Dependency> dependencies) { this.dependencies = dependencies; }
+  public void setDepends(List<List<String>> depends) { this.depends = depends; }
   public void setConflicts(List<String> conflicts) { this.conflicts = conflicts; }
 }
 
@@ -41,8 +35,18 @@ public class Main {
     List<String> initial = JSON.parseObject(readFile(args[1]), strListType);
     List<String> constraints = JSON.parseObject(readFile(args[2]), strListType);
 
-    // CONTINUE HERE: using repo, initial and constraints, compute a solution
-    // and print the answer
+    // CHANGE CODE BELOW:
+    // using repo, initial and constraints, compute a solution and print the answer
+    for (Package p : repo) {
+      System.out.printf("package %s version %s\n", p.getName(), p.getVersion());
+      for (List<String> clause : p.getDepends()) {
+        System.out.printf("  dep:");
+        for (String q : clause) {
+          System.out.printf(" %s", q);
+        }
+        System.out.printf("\n");
+      }
+    }
   }
 
   static String readFile(String filename) throws IOException {
