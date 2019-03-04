a142e9e6acdf02070d3a23b49ffb770cb7033dec
Damon Sweeney <ds576@kent.ac.uk>
2019-03-04 16:18:27 +0000

    Remove java boilerplate from install deps

--- install_deps.sh
+++ install_deps.sh
@@ -6,9 +6,3 @@ pip3 install numpy
 pip3 install networkx
 pip3 install satispy
 pip3 install toposort
-
-
-rm -rf lib/*
-mkdir -p lib
-wget -O lib/fastjson-1.2.45.jar http://search.maven.org/remotecontent?filepath=com/alibaba/fastjson/1.2.45/fastjson-1.2.45.jar
-
